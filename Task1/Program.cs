04a70aee83d7aae20820b93c16d5a95e0fc8aa1b
ch1llay <ch1llay@yandex.ru>
2021-10-15 13:36:29 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 5;
+                int b = 0;
+                int y = a / b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
