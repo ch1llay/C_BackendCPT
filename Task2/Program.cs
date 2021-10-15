9bcc2a9d8323b4d1f1009e761fb38b3f8ef23a47
ch1llay <ch1llay@yandex.ru>
2021-10-15 22:48:05 +0300

    Task2 is complete P.S. Linq is just magic

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Task2
 {
@@ -38,41 +39,27 @@ namespace Task2
             Departament departament1 = new Departament("Departament1", 1, "Manager1");
             Departament departament2 = new Departament("Departament2", 2, "Manager2");
             Departament departament3 = new Departament("Departament3", 3, "Manager3");
+
             List < Employee > everythingEmployees = new List<Employee>();
+
             everythingEmployees.Add(new Employee("Иванов", "Иван", "Иванович", departament1));
             everythingEmployees.Add(new Employee("Петров", "Петр", "Петрович", departament2));
             everythingEmployees.Add(new Employee("Сидоров", "Иван", "Петрович", departament3));
             everythingEmployees.Add(new Employee("Петров", "Иван", "Иванович", departament1));
             everythingEmployees.Add(new Employee("Иванов", "Петр", "Петрович", departament2));
             everythingEmployees.Add(new Employee("Петров", "Иван", "Петрович", departament3));
+
             Dictionary<string, List<Employee>> departamentEmploeyes = new Dictionary<string, List<Employee>>();
 
-            List<Employee> departament1Emloeyees = new List<Employee>();
-            List<Employee> departament2Emloeyees = new List<Employee>();
-            List<Employee> departament3Emloeyees = new List<Employee>();
+            departamentEmploeyes.Add(departament1.title, everythingEmployees.Where(el => el.departament.title.StartsWith(departament1.title)).ToList());
+            departamentEmploeyes.Add(departament2.title, everythingEmployees.Where(el => el.departament.title.StartsWith(departament2.title)).ToList());
+            departamentEmploeyes.Add(departament3.title, everythingEmployees.Where(el => el.departament.title.StartsWith(departament3.title)).ToList());
 
-            foreach(var employee in everythingEmployees)
+            foreach(KeyValuePair<string, List<Employee>> keyValue in departamentEmploeyes)
             {
-                switch (employee.departament.title)
-                {
-                    case "Departament1":
-                        departament1Emloeyees.Add(employee);
-                        break;
-                    case "Departament2":
-                        departament2Emloeyees.Add(employee);
-                        break;
-                    case "Departament3":
-                        departament3Emloeyees.Add(employee);
-                        break;
-
-                }
+                Console.WriteLine("Отдел " + keyValue.Key);
+                Console.WriteLine(string.Join(Environment.NewLine, keyValue.Value.Select(x=> $"\t {x.surname} {x.name} {x.patronymic}")));
             }
-            departamentEmploeyes.Add("Departament1", departament1Emloeyees);
-            departamentEmploeyes.Add("Departament2", departament2Emloeyees);
-            departamentEmploeyes.Add("Departament3", departament3Emloeyees);
-
-
-
 
         }
     }
