2f8a7817e0b75e5fec10ef38dcb47f3fe7a3655e
ch1llay <ch1llay@yandex.ru>
2021-10-15 15:07:23 +0300

    add class Departament and Employee

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,6 +2,21 @@
 
 namespace Task2
 {
+
+    class Departament
+    {
+        string title;
+        int number;
+        string manager;
+    }
+    class Employee
+    {
+        string surname;
+        string name;
+        string patronymic;
+        Departament departament;
+
+    }
     class Program
     {
         static void Main(string[] args)
