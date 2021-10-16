04423fc71f3ba939bd41450f456ed446b3d9b890
ch1llay <ch1llay@yandex.ru>
2021-10-16 19:57:47 +0300

    Task3 is complete

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task3
+{
+    interface IAnimal
+    {
+        public void MakeSound();
+    }
+
+    class Cat : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    class Dog : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Woof-Woof");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.MakeSound();
+
+            Dog dog = new Dog();
+            dog.MakeSound();
+        }
+    }
+}
